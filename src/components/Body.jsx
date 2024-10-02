59502efe6d613b78b7b12a7c76cb22b2d86219d5
shobhit0111 <shobhitbhatnagar61@gmail.com>
2024-10-02 20:33:55 +0530

    Changes done in src component files

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,33 @@
 import RestoCard from "./RestoCard";
 import resList from "../Utils/restoData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {resList.map((restaurant) => (
-            <RestoCard key={restaurant.id} resData={restaurant} />
-          ))}
-        </div>
-      </div>
-    );
-};
-export default Body;
+    //Local State Variable
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
   
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.avgRating > 4.1
+            );
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestoCard key={restaurant.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
